6371dd4b9f97124324e5079afd564c7da225a461
Charles1026 <xiaozhehao@gmail.com>
2024-09-06 14:41:30 +0800

    move out common functions and constants

--- ass1/1_tester.py
+++ ass1/1_tester.py
@@ -3,85 +3,7 @@ import paramiko
 import tqdm
 import re
 
-PIPELINE_WIDTH_IDX = 0
-INT_ALU_IDX = 1
-INT_MULTIPLIER_IDX = 2
-FP_ALU_IDX = 3
-FP_MULTIPLIER_IDX = 4
-OUT_OF_ORDER_ISSUE_IDX = 5
-REORDER_BUFFER_IDX = 6
-LOAD_STORE_QUEUE_IDX = 7
-
-MAX_AREA = 60.0
-SSH_USERNAME = "zhehao"
-
-VALUE_RANGE = [
-  (0, 2),
-  (1, 4),
-  (1, 1),
-  (1, 1),
-  (1, 4),
-  (0, 1),
-  (3, 5),
-  (2, 4)
-]
-
-VALUE_TO_CONFIG = [
-  lambda state: (2 ** state[PIPELINE_WIDTH_IDX]),
-  lambda state: state[INT_ALU_IDX],
-  lambda state: state[INT_MULTIPLIER_IDX],
-  lambda state: state[FP_ALU_IDX],
-  lambda state: state[FP_MULTIPLIER_IDX],
-  lambda state: 0 if state[OUT_OF_ORDER_ISSUE_IDX] else 1,
-  lambda state: 2 ** (state[REORDER_BUFFER_IDX]),
-  lambda state: 2 ** (state[LOAD_STORE_QUEUE_IDX]),
-]
-
-VALUE_TO_AREA = [
-  lambda state: (2 ** state[PIPELINE_WIDTH_IDX]) * 1.8,
-  lambda state: state[INT_ALU_IDX] * 2,
-  lambda state: state[INT_MULTIPLIER_IDX] * 3,
-  lambda state: state[FP_ALU_IDX] * 4,
-  lambda state: state[FP_MULTIPLIER_IDX] * 5,
-  lambda state: state[OUT_OF_ORDER_ISSUE_IDX] * 1.4 * (
-    VALUE_TO_AREA[PIPELINE_WIDTH_IDX](state) + VALUE_TO_AREA[INT_ALU_IDX](state)
-    + VALUE_TO_AREA[INT_MULTIPLIER_IDX](state) + VALUE_TO_AREA[FP_ALU_IDX](state)
-    + VALUE_TO_AREA[FP_MULTIPLIER_IDX](state)),
-  lambda state: state[OUT_OF_ORDER_ISSUE_IDX] * (2 ** (state[REORDER_BUFFER_IDX] - 1)),
-  lambda state: state[OUT_OF_ORDER_ISSUE_IDX] * (2 ** (state[LOAD_STORE_QUEUE_IDX] - 1)),
-]
-
-def build_state(index: int):
-  if index == (len(VALUE_RANGE) - 1):
-    return [[i] for i in range(VALUE_RANGE[index][0], VALUE_RANGE[index][1] + 1)]
-
-  ret_list = []
-  for val in range(VALUE_RANGE[index][0], VALUE_RANGE[index][1] + 1):
-    sub_list = build_state(index + 1)
-    ret_list += map(lambda state : [val] + state, sub_list)
-  
-  return ret_list
-
-def state_to_config_values(state: np.ndarray):
-  list_state = state.tolist()
-  config_values = []
-  for idx in range(len(list_state)):
-    config_values.append((VALUE_TO_CONFIG[idx](list_state)))
-  
-  return config_values
-    
-def generate_command(config_values) -> str:
-  cmd = f"srun sim-outorder -config ~/CS4223/assignment1/min.cfg " \
-    f"-decode:width {config_values[PIPELINE_WIDTH_IDX]} -issue:width {config_values[PIPELINE_WIDTH_IDX]} -commit:width {config_values[PIPELINE_WIDTH_IDX]} " \
-    f"-res:ialu {config_values[INT_ALU_IDX]} " \
-    f"-res:imult {config_values[INT_MULTIPLIER_IDX]} " \
-    f"-res:fpalu {config_values[FP_ALU_IDX]} " \
-    f"-res:fpmult {config_values[FP_MULTIPLIER_IDX]} " \
-    f"-issue:inorder {config_values[OUT_OF_ORDER_ISSUE_IDX]} " \
-    f"-ruu:size {config_values[REORDER_BUFFER_IDX]} " \
-    f"-lsq:size {config_values[LOAD_STORE_QUEUE_IDX]} " \
-    "~/CS4223/assignment1/benchmarks/compress95.ss < ~/CS4223/assignment1/benchmarks/compress95.in > ~/CS4223/assignment1/benchmarks/compress95.out"
-  return cmd
+from common import *
 
 if __name__ == "__main__":
   ### Build all possible states ###
@@ -127,7 +49,7 @@ if __name__ == "__main__":
   optimal_config_values = []
   optimal_ipcs = []
   # Open result log file
-  with open("./res.log", "a") as res_file:
+  with open("./res_1.log", "a") as res_file:
     # Establish ssh connection
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
@@ -151,7 +73,7 @@ if __name__ == "__main__":
       # Send and process result
       stdin, stdout, stderr = ssh.exec_command(cmd)
       result = stderr.read().decode()
-      match = re.search(r'sim_IPC.*\d+\.\d+', result)
+      match = re.search(SIM_IPC_PATTERN, result)
       
       # Extract the ipc value
       if match:
---
+++ ass1/common.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+PIPELINE_WIDTH_IDX = 0
+INT_ALU_IDX = 1
+INT_MULTIPLIER_IDX = 2
+FP_ALU_IDX = 3
+FP_MULTIPLIER_IDX = 4
+OUT_OF_ORDER_ISSUE_IDX = 5
+REORDER_BUFFER_IDX = 6
+LOAD_STORE_QUEUE_IDX = 7
+
+MAX_AREA = 60.0
+SSH_USERNAME = "zhehao"
+
+SIM_IPC_PATTERN = r'sim_IPC.*\d+\.\d+'
+
+VALUE_RANGE = [
+  (0, 2),
+  (1, 4),
+  (1, 1),
+  (1, 1),
+  (1, 4),
+  (0, 1),
+  (3, 5),
+  (2, 4)
+]
+
+VALUE_TO_CONFIG = [
+  lambda state: (2 ** state[PIPELINE_WIDTH_IDX]),
+  lambda state: state[INT_ALU_IDX],
+  lambda state: state[INT_MULTIPLIER_IDX],
+  lambda state: state[FP_ALU_IDX],
+  lambda state: state[FP_MULTIPLIER_IDX],
+  lambda state: 0 if state[OUT_OF_ORDER_ISSUE_IDX] else 1,
+  lambda state: 2 ** (state[REORDER_BUFFER_IDX]),
+  lambda state: 2 ** (state[LOAD_STORE_QUEUE_IDX]),
+]
+
+VALUE_TO_AREA = [
+  lambda state: (2 ** state[PIPELINE_WIDTH_IDX]) * 1.8,
+  lambda state: state[INT_ALU_IDX] * 2,
+  lambda state: state[INT_MULTIPLIER_IDX] * 3,
+  lambda state: state[FP_ALU_IDX] * 4,
+  lambda state: state[FP_MULTIPLIER_IDX] * 5,
+  lambda state: state[OUT_OF_ORDER_ISSUE_IDX] * 1.4 * (
+    VALUE_TO_AREA[PIPELINE_WIDTH_IDX](state) + VALUE_TO_AREA[INT_ALU_IDX](state)
+    + VALUE_TO_AREA[INT_MULTIPLIER_IDX](state) + VALUE_TO_AREA[FP_ALU_IDX](state)
+    + VALUE_TO_AREA[FP_MULTIPLIER_IDX](state)),
+  lambda state: state[OUT_OF_ORDER_ISSUE_IDX] * (2 ** (state[REORDER_BUFFER_IDX] - 1)),
+  lambda state: state[OUT_OF_ORDER_ISSUE_IDX] * (2 ** (state[LOAD_STORE_QUEUE_IDX] - 1)),
+]
+
+################## Functions ##################
+def build_state(index: int):
+  if index == (len(VALUE_RANGE) - 1):
+    return [[i] for i in range(VALUE_RANGE[index][0], VALUE_RANGE[index][1] + 1)]
+
+  ret_list = []
+  for val in range(VALUE_RANGE[index][0], VALUE_RANGE[index][1] + 1):
+    sub_list = build_state(index + 1)
+    ret_list += map(lambda state : [val] + state, sub_list)
+  
+  return ret_list
+
+def state_to_config_values(state: np.ndarray):
+  list_state = state.tolist()
+  config_values = []
+  for idx in range(len(list_state)):
+    config_values.append((VALUE_TO_CONFIG[idx](list_state)))
+  
+  return config_values
+    
+def generate_command(config_values) -> str:
+  cmd = f"srun sim-outorder -config ~/CS4223/assignment1/min.cfg " \
+    f"-decode:width {config_values[PIPELINE_WIDTH_IDX]} -issue:width {config_values[PIPELINE_WIDTH_IDX]} -commit:width {config_values[PIPELINE_WIDTH_IDX]} " \
+    f"-res:ialu {config_values[INT_ALU_IDX]} " \
+    f"-res:imult {config_values[INT_MULTIPLIER_IDX]} " \
+    f"-res:fpalu {config_values[FP_ALU_IDX]} " \
+    f"-res:fpmult {config_values[FP_MULTIPLIER_IDX]} " \
+    f"-issue:inorder {config_values[OUT_OF_ORDER_ISSUE_IDX]} " \
+    f"-ruu:size {config_values[REORDER_BUFFER_IDX]} " \
+    f"-lsq:size {config_values[LOAD_STORE_QUEUE_IDX]} " \
+    "~/CS4223/assignment1/benchmarks/compress95.ss < ~/CS4223/assignment1/benchmarks/compress95.in > ~/CS4223/assignment1/benchmarks/compress95.out"
+  return cmd
